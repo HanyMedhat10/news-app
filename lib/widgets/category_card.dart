9bcdb24300d5c7aa52d9ebc5f0b282d5b30b1904
Hany Medhat <hany.medhat24@gmail.com>
2023-08-25 20:33:10 +0300

    create category model and display all categories

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/category_model.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({
     super.key,
+    required this.categories,
   });
-
+  final CategoryModel categories;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right:16.0),
+      padding: const EdgeInsets.only(right: 16.0),
       child: Container(
         height: 85,
         width: 160,
         decoration: BoxDecoration(
-          image: const DecorationImage(
+          image: DecorationImage(
             fit: BoxFit.fill,
-            image: AssetImage('assets/sports.avif'),
+            image: AssetImage(categories.image),
           ),
           borderRadius: BorderRadius.circular(12),
         ),
-        child: const Center(
+        child: Center(
           child: Text(
-            'Sports',
-            style: TextStyle(
+            categories.categoryName,
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 16,
               fontWeight: FontWeight.bold,
--- lib/widgets/category_list_view.dart
+++ lib/widgets/category_list_view.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/category_model.dart';
 import 'package:news_app/widgets/category_card.dart';
 
 class CategoryListView extends StatelessWidget {
   const CategoryListView({
     super.key,
   });
-
+  final List<CategoryModel> categories = const [
+    CategoryModel(image: 'assets/business.avif', categoryName: 'Business'),
+    CategoryModel(image: 'assets/general.avif', categoryName: 'General'),
+    CategoryModel(image: 'assets/sports.avif', categoryName: 'Spots'),
+    CategoryModel(image: 'assets/health.avif', categoryName: 'Health'),
+    CategoryModel(image: 'assets/science.avif', categoryName: 'Science'),
+    CategoryModel(image: 'assets/technology.jpeg', categoryName: 'Technology'),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 85,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
+        itemCount: categories.length,
         itemBuilder: (context, index) {
-          return const CategoryCard();
+          return  CategoryCard(
+            categories: categories[index],
+          );
         },
       ),
     );
