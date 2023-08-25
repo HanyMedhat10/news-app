9deefa0867246ba7875608f7984a29337e526abe
Hany Medhat <hany.medhat24@gmail.com>
2023-08-25 21:06:27 +0300

    create news list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/widgets/news_tile.dart';
 
-import '../widgets/category_list_view.dart';
+import '../widgets/news_list_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -27,7 +28,8 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
-      body: const CategoryListView(),
+      body: const NewsListView(),
     );
   }
 }
+
