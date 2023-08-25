749ca59a1ff25e33642ebd4179b0f4415acba215
Hany Medhat <hany.medhat24@gmail.com>
2023-08-25 19:44:22 +0300

    Create app bar

---
+++ lib/views/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: const Column(),
+    );
+  }
+}
