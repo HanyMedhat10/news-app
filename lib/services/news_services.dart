4ab1b48cb12ef39c754ed823ddeb2ce07210243c
Hany Medhat <hany.medhat24@gmail.com>
2023-08-29 22:26:47 +0300

    add dio and create news services

---
+++ lib/services/news_services.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class NewsService {
+  // final dio =  Dio();
+  final Dio dio;
+  final String apiKey = '9cdd34ed241641fe8a429bec1adc5b85';
+  final String baseApi = 'https://newsapi.org/v2/everything?';
+  final String path =
+      'https://newsapi.org/v2/everything?country=eg&apiKey=9cdd34ed241641fe8a429bec1adc5b85&category=general';
+  NewsService(this.dio);
+
+  void getNews() async {
+    Response response = await dio.get(path);
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData['articles'];
+    print(articles);
+  }
+}
