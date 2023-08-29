1735cf2b3833ce6ea0439d617c770f3bc6352d3b
Hany Medhat <hany.medhat24@gmail.com>
2023-08-30 01:37:41 +0300

    Finish getNews method

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -7,13 +7,22 @@ class NewsService {
   final String apiKey = '9cdd34ed241641fe8a429bec1adc5b85';
   final String baseApi = 'https://newsapi.org/v2/everything?';
   final String path =
-      'https://newsapi.org/v2/everything?country=eg&apiKey=9cdd34ed241641fe8a429bec1adc5b85&category=general';
+      'https://newsapi.org/v2/everything?country=us&apiKey=9cdd34ed241641fe8a429bec1adc5b85&category=general';
   NewsService(this.dio);
 
-  void getNews() async {
+  Future<List<ArticleModel>> getNews() async {
     Response response = await dio.get(path);
     Map<String, dynamic> jsonData = response.data;
     List<dynamic> articles = jsonData['articles'];
-   
+    List<ArticleModel> articleList = [];
+    for (var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+        image: article['urlToImage'],
+        title: article['title'],
+        subTitle: article['description'],
+      );
+      articleList.add(articleModel);
+    }
+    return articleList;
   }
 }
